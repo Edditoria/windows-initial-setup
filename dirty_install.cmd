5ad967de42497290d4bbd05e45b7f7e9f446a84c
Edditoria <edditoria@gmail.com>
2017-10-18 20:22:16 +0800

    [feat: CDBurnerXP] add import options file into registry

--- dirty_install.cmd
+++ dirty_install.cmd
@@ -125,5 +125,6 @@ echo.
 echo Installing CDBurnerXP x86...
 set install_args=/silent /sp- /norestart /q /v"ALLUSERS=2 /qn" /loadinf^=%recipes_dir%cdburnerxp\options.cfg
 start /w "" %downloads_dir%%cdburnerxp_filename% %install_args%
-:: #todo setting options
+echo Setting options...
+reg import %recipes_dir%cdburnerxp\options.reg
 echo [done]
