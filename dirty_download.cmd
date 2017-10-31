4a614e170d02727a49e729557bb6ff8550c9269d
Edditoria <edditoria@gmail.com>
2017-10-31 17:56:38 +0800

    [feat-download] add download all other installation files (total 8 of 9)

--- dirty_download.cmd
+++ dirty_download.cmd
@@ -15,6 +15,14 @@ set "this_dir=%cd%"
 cd %downloads_dir%
 
 call :func_download "7-Zip" "%a_7_zip_x64_download_url%" "%a_7_zip_x64_filename%"
+call :func_download "Adobe Acrobat Reader DC" "%adobe_acrobat_reader_download_url%" "%adobe_acrobat_reader_filename%"
+call :func_download "Adobe Flash NPAPI" "%flash_npapi_download_url%" "%flash_npapi_filename%"
+call :func_download "CDBurnerXP" "%cdburnerxp_download_url%" "%cdburnerxp_filename%"
+call :func_download "Google Chrome" "%chrome_download_url%" "%chrome_filename%"
+call :func_download "JRE (Java Runtime) x86" "%jre_x86_download_url%" "%jre_x86_filename%"
+call :func_download "JRE (Java Runtime) x64" "%jre_x64_download_url%" "%jre_x64_filename%"
+call :func_download "LibreOffice" "%libreoffice_download_url%" "%libreoffice_filename%"
+call :func_download "VLC Media Player" "%vlc_download_url%" "%vlc_filename%"
 
 
 :: Actions after finishing download
