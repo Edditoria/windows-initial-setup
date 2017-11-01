0e97f92867cb134f20d29b1e53a38d759e37a116
Edditoria <edditoria@gmail.com>
2017-11-01 14:18:34 +0800

    [fix-google-chrome] download en-US or zh-HK installation file according to argument %lang%

--- dirty_download.cmd
+++ dirty_download.cmd
@@ -46,7 +46,11 @@ cd %downloads_dir%
 call :func_download "%a_7_zip_x64_fullname%" "%a_7_zip_x64_download_url%" "%a_7_zip_x64_filename%"
 call :func_download "%flash_npapi_fullname%" "%flash_npapi_download_url%" "%flash_npapi_filename%"
 call :func_download "%cdburnerxp_fullname%" "%cdburnerxp_download_url%" "%cdburnerxp_filename%"
-call :func_download "%google_chrome_x64_fullname%" "%google_chrome_x64_download_url%" "%google_chrome_x64_filename%"
+if "%lang%"=="zh-HK" (
+	call :func_download "%google_chrome_x64_fullname%" "%google_chrome_x64_download_url%" "%google_chrome_x64_filename%"
+) else (
+	call :func_download "%google_chrome_x64_fullname%" "%google_chrome_x64_zhHK_download_url%" "%google_chrome_x64_filename%"
+)
 call :func_download "%java_x86_fullname%" "%java_x86_download_url%" "%java_x86_filename%"
 call :func_download "%java_x64_fullname%" "%java_x64_download_url%" "%java_x64_filename%"
 call :func_download "%libreoffice_fullname%" "%libreoffice_download_url%" "%libreoffice_filename%"
