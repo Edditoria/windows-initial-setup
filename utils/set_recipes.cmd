22995bdb7c3470a6818581543600bc47782c0915
Edditoria <edditoria@gmail.com>
2018-04-01 15:58:04 +0800

    #recipe-all update download URLs

--- utils/set_recipes.cmd
+++ utils/set_recipes.cmd
@@ -16,8 +16,8 @@ if not exist "%repo_dir%%nul%" (
 
 set "a_7_zip_recipe_name=7-zip"
 set "a_7_zip_x64_fullname=7-Zip (x64)"
-set "a_7_zip_x64_filename=7z1604-x64.exe"
-set "a_7_zip_x64_download_url=http://www.7-zip.org/a/7z1604-x64.exe"
+set "a_7_zip_x64_filename=7z1801-x64.exe"
+set "a_7_zip_x64_download_url=https://www.7-zip.org/a/7z1801-x64.exe"
 
 set "adobe_acrobat_reader_recipe_name=adobe-acrobat-reader"
 set "adobe_acrobat_reader_fullname=Adobe Acrobat Reader DC"
@@ -26,8 +26,8 @@ set "adobe_acrobat_reader_download_url=ftp://ftp.adobe.com/pub/adobe/reader/win/
 
 set "cdburnerxp_recipe_name=cdburnerxp"
 set "cdburnerxp_fullname=CDBurnerXP"
-set "cdburnerxp_filename=cdbxp_setup_4.5.7.6623.exe"
-set "cdburnerxp_download_url=https://download.cdburnerxp.se/cdbxp_setup_4.5.7.6623.exe"
+set "cdburnerxp_filename=cdbxp_setup_4.5.8.6795.exe"
+set "cdburnerxp_download_url=https://download.cdburnerxp.se/cdbxp_setup_4.5.8.6795.exe"
 
 set "flash_npapi_recipe_name=flash-npapi"
 set "flash_npapi_fullname=Adobe Flash Player NPAPI"
@@ -41,18 +41,18 @@ set "google_chrome_x64_download_url=https://dl.google.com/tag/s/dl/chrome/instal
 
 set "java_recipe_name=java"
 set "java_x86_fullname=Java Runtime (JRE) (x86)"
-set "java_x86_filename=jre-8u151-windows-i586.exe"
-set "java_x86_download_url=http://javadl.oracle.com/webapps/download/AutoDL?BundleId=227550_e758a0de34e24606bca991d704f6dcbf"
+set "java_x86_filename=jre-8u161-windows-i586.exe"
+set "java_x86_download_url=http://javadl.oracle.com/webapps/download/AutoDL?BundleId=230540_2f38c3b165be4555a1fa6e98c45e0808"
 set "java_x64_fullname=Java Runtime (JRE) (x64)"
-set "java_x64_filename=jre-8u151-windows-x64.exe"
-set "java_x64_download_url=http://javadl.oracle.com/webapps/download/AutoDL?BundleId=227552_e758a0de34e24606bca991d704f6dcbf"
+set "java_x64_filename=jre-8u161-windows-x64.exe"
+set "java_x64_download_url=http://javadl.oracle.com/webapps/download/AutoDL?BundleId=230542_2f38c3b165be4555a1fa6e98c45e0808"
 
 set "libreoffice_recipe_name=libreoffice"
 set "libreoffice_fullname=LibreOffice"
-set "libreoffice_filename=LibreOffice_5.3.7_Win_x86.msi"
-set "libreoffice_download_url=http://download.documentfoundation.org/libreoffice/stable/5.3.7/win/x86/LibreOffice_5.3.7_Win_x86.msi"
+set "libreoffice_filename=LibreOffice_5.4.6_Win_x86.msi"
+set "libreoffice_download_url=http://download.documentfoundation.org/libreoffice/stable/5.4.6/win/x86/LibreOffice_5.4.6_Win_x86.msi"
 
 set "vlc_recipe_name=vlc"
 set "vlc_x64_fullname=VLC Media Player"
-set "vlc_x64_filename=vlc-2.2.6-win64.exe"
-set "vlc_x64_download_url=https://mirror.vinahost.vn/videolan/vlc/2.2.6/win64/vlc-2.2.6-win64.exe"
+set "vlc_x64_filename=vlc-3.0.1-win64.exe"
+set "vlc_x64_download_url=https://mirror.vinahost.vn/videolan/vlc/3.0.1/win64/vlc-3.0.1-win64.exe"
