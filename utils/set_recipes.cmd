d81767c48bb4ce3f747b9ea151bb0674f52b3e29
Edditoria <edditoria@gmail.com>
2020-01-27 22:31:10 +0800

    Update all recipes to latest version

--- utils/set_recipes.cmd
+++ utils/set_recipes.cmd
@@ -26,8 +26,8 @@ set "adobe_acrobat_reader_download_url=http://ardownload.adobe.com/pub/adobe/rea
 
 set "cdburnerxp_recipe_name=cdburnerxp"
 set "cdburnerxp_fullname=CDBurnerXP"
-set "cdburnerxp_filename=cdbxp_setup_4.5.8.7042.exe"
-set "cdburnerxp_download_url=https://download.cdburnerxp.se/cdbxp_setup_4.5.8.7042.exe"
+set "cdburnerxp_filename=cdbxp_setup_4.5.8.7128_minimal.exe"
+set "cdburnerxp_download_url=https://download.cdburnerxp.se/cdbxp_setup_4.5.8.7128_minimal.exe"
 
 set "flash_npapi_recipe_name=flash-npapi"
 set "flash_npapi_fullname=Adobe Flash Player NPAPI"
@@ -41,18 +41,18 @@ set "google_chrome_x64_download_url=https://dl.google.com/tag/s/dl/chrome/instal
 
 set "java_recipe_name=java"
 set "java_x86_fullname=Java Runtime (JRE) (x86)"
-set "java_x86_filename=jre-8u201-windows-i586.exe"
-set "java_x86_download_url=https://javadl.oracle.com/webapps/download/AutoDL?BundleId=236886_42970487e3af4f5aa5bca3f542482c60"
+set "java_x86_filename=jre-8u241-windows-i586.exe"
+set "java_x86_download_url=https://javadl.oracle.com/webapps/download/AutoDL?BundleId=241534_1f5b5a70bf22433b84d0e960903adac8"
 set "java_x64_fullname=Java Runtime (JRE) (x64)"
-set "java_x64_filename=jre-8u201-windows-x64.exe"
-set "java_x64_download_url=https://javadl.oracle.com/webapps/download/AutoDL?BundleId=236888_42970487e3af4f5aa5bca3f542482c60"
+set "java_x64_filename=jre-8u241-windows-x64.exe"
+set "java_x64_download_url=https://javadl.oracle.com/webapps/download/AutoDL?BundleId=241536_1f5b5a70bf22433b84d0e960903adac8"
 
 set "libreoffice_recipe_name=libreoffice"
 set "libreoffice_fullname=LibreOffice"
-set "libreoffice_filename=LibreOffice_6.1.5_Win_x86.msi"
-set "libreoffice_download_url=https://download.documentfoundation.org/libreoffice/stable/6.1.5/win/x86/LibreOffice_6.1.5_Win_x86.msi"
+set "libreoffice_filename=LibreOffice_6.2.8_Win_x86.msi"
+set "libreoffice_download_url=https://download.documentfoundation.org/libreoffice/stable/6.2.8/win/x86/LibreOffice_6.2.8_Win_x86.msi"
 
 set "vlc_recipe_name=vlc"
 set "vlc_x64_fullname=VLC Media Player"
-set "vlc_x64_filename=vlc-3.0.6-win64.exe"
-set "vlc_x64_download_url=https://get.videolan.org/vlc/3.0.6/win64/vlc-3.0.6-win64.exe"
+set "vlc_x64_filename=vlc-3.0.8-win64.exe"
+set "vlc_x64_download_url=https://get.videolan.org/vlc/3.0.8/win64/vlc-3.0.8-win64.exe"
